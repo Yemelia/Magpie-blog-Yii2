eddf82601a85e5d82adfbb221cb218fb70652ebb
Nikita Yemelianov <yemelianovnk@gmail.com>
2017-04-22 16:42:51 +0300

    add database migrate

---
+++ migrations/m170422_133647_create_add_date_to_comment.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+class m170422_133647_create_add_date_to_comment extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('comment','date', $this->date());
+    }
+
+    public function down()
+    {
+        $this->dropColumn('comment','date');
+
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
